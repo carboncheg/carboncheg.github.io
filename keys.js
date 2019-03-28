def3dd52fb13dac7680c131c95c0fe11c34e3f26
Sergei <garibzyanov@gmail.com>
2019-03-28 23:09:10 +0500

    change mongoURI

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://sergei:01s1989@fullstack-blog-ws5pm.mongodb.net/test?retryWrites=true'
-    // mongoURI: 'mongodb://vladilen:123456@ds245228.mlab.com:45228/fullstack-blog'
+    // mongoURI: 'mongodb+srv://sergei:01s1989@fullstack-blog-ws5pm.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb://vladilen:123456@ds245228.mlab.com:45228/fullstack-blog'
   };
